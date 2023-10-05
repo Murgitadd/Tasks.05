8b10530377c3483190f47db2a97b21eabb2c1274
Murad <145739695+Murgitadd@users.noreply.github.com>
2023-10-05 23:02:57 +0400

    Done Task2.cs

--- Task2.cs
+++ Task2.cs
@@ -1,16 +1,71 @@
-Console.WriteLine("Enter a three-digit number:");
+using System;
 
-int num = int.Parse(Console.ReadLine());
+class Program {
+  
+  public static void Main (string[] args) {
+    ATM();
+  }
 
-int yuzluk = (num % 1000) / 100;
-int onluq = (num % 100) / 10;
-int teklik = (num % 10);
+  static void ATM ()
+  { 
+    Console.WriteLine("===========");
+    Console.WriteLine("~~~ATM~~~");
+    Console.WriteLine("===========");
 
-if (yuzluk == onluq && onluq == teklik)
-{
-    Console.WriteLine("Butun ededler eynidir.");
-}
-else
-{
-    Console.WriteLine("Eyni olmayan ededler var!");
+    
+    double balance = 0;
+    bool check = true;
+
+while (check) {
+    Console.WriteLine("Emeliyyatı Seç:");
+    Console.WriteLine("[1] Balansi goster.");
+    Console.WriteLine("[2] Pul cixar.");
+    Console.WriteLine("[3] Pul elave et.");
+    Console.WriteLine("[0] Cix.");
+  
+    Console.WriteLine("");
+    string choose = Console.ReadLine();
+    Console.WriteLine("");
+  
+    if (choose=="1")
+    {
+      Console.WriteLine($"Balans:{balance}");
+    }
+    else if (choose=="2")
+    {
+      Console.WriteLine("Meblegi daxil et");
+      Console.Write("Mebleg: ");
+      double withdraw=double.Parse(Console.ReadLine());
+      Console.WriteLine("");
+      if (balance < withdraw)
+      {
+        Console.WriteLine("Lazim qeder Pulun yoxdur.");
+      }
+      else
+      {
+        balance -= withdraw;
+      }
+    }
+    else if (choose=="3")
+    {
+      Console.WriteLine("Meblegi elave et");
+      Console.Write("Mebleg: ");
+      double add=double.Parse(Console.ReadLine());
+      Console.WriteLine("");
+      balance += add;
+    }
+    else if (choose=="0")
+    {
+      Console.WriteLine("Bizi secdiyiniz ucun sag ol.");
+      check = false;
+    }
+    else
+    {
+      Console.WriteLine("Duzgun secim et.");
+    }
+  Console.WriteLine("----------------");
+   }
+  }
+
+  
 }
